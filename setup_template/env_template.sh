9fbf0899aa86635b962b2e020ca5a4065562c76b
Chris Healer <chris@eyelash.ai>
2023-03-25 11:16:58 -0400

    config comment

--- setup_template/env_template.sh
+++ setup_template/env_template.sh
@@ -12,7 +12,7 @@ export PROJECT_MAIN_PAGE='../pages/README.md'				# the main home markdown page f
 export DOXYGEN_FILTER='../scripts/XXX_filter.py'			# a script filter for interpreting the code (adds stuff like decorators or macros)
 export DOXYGEN_INPUT='../../src "../../Folder With Spaces"'		# a list of input folders for documenting, which is a whitespace-separated list of (optionally) quoted paths.  The Doxyfile will already include "../pages" for you.  Paths are relative to the `docs-doxygen` folder.
 export DOXYGEN_STRIP_FROM_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../Path/To/Base" && pwd)"	# the full absolute path to the root folder to be removed during publish.  This is kind of cosmetic but also less confusing.
-export DOXYGEN_IGNORE_PREFIX=''						# a class/method prefix to be ignored.  For instance if everything is pApexSDK, mMatrix, etc, the ignore prefix may want to be "p\nm" where the list is a space-separated list of prefixes.
+export DOXYGEN_IGNORE_PREFIX=''						# a class/method prefix to be ignored.  For instance if everything is pApexSDK, mMatrix, etc, the ignore prefix may want to be "p\nm" where the list is a space-separated list of prefixes.  The ignore order matters!
 
 # user variables.  Useful when writing pages that refer to other urls, but don't want to hardcode them in.
 export APEX_SERVER_URL='https://apex.pixovr.com'
