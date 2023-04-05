4a4dbb38da592dcd603312e63f9fd36dae3b7501
Chris Healer <chris@eyelash.ai>
2023-04-05 00:47:17 -0600

    working out docs-docker-base

--- setup_doxygen.sh
+++ setup_doxygen.sh
@@ -3,16 +3,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-git clone https://github.com/doxygen/doxygen.git
+# exit on errors
+set -e
+
+git clone --recurse-submodules https://github.com/doxygen/doxygen.git
 cd doxygen
 
 mkdir build
 cd build
 cmake -G "Unix Makefiles" ..
 make
+#make -j
 
 # print version
-#./bin/doxygen -v
+./bin/doxygen -v
+
+cd $DIR
 
 chmod 755 scripts/* 
 
