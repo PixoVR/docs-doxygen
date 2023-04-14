c2cd07b5e9721d5b33a86c4ad67d24213938c46a
Chris Healer <chris@eyelash.ai>
2023-04-14 09:03:42 -0600

    build comment

--- setup_doxygen.sh
+++ setup_doxygen.sh
@@ -13,8 +13,8 @@ mkdir build
 cd build
 cmake -G "Unix Makefiles" ..
 #make
-#make -j		# unstable on some configurations!
-make -j 8		# unstable on some configurations!
+#make -j		# unstable on some configurations!  If it runs out of memory, jobs will crash.
+make -j 8
 
 #ln -s $PWD/bin/doxygen /usr/local/bin/doxygen
 cp bin/doxygen /usr/local/bin/doxygen
