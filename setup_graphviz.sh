a8f98266d98effbc19fdb2ab10da8fbf3a9503a9
Chris Healer <chris@eyelash.ai>
2023-07-06 16:54:11 -0600

    cmake from gitlab

--- setup_graphviz.sh
+++ setup_graphviz.sh
@@ -9,16 +9,17 @@ set -e
 #wget https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/8.0.5/graphviz-8.0.5.tar.gz
 #tar -zxf graphviz-8.0.5.tar.gz
 
-git clone --recurse-submodules https://github.com/graphviz/graphviz.git
-
+git clone --recurse-submodules https://gitlab.com/graphviz/graphviz.git
 cd graphviz
-./configure
+
+mkdir build
+cd build
+cmake ../
 make -j 8
 make install
 
-cd $DIR
-
 # cleanup
+cd $DIR
 rm -rv graphviz
 
 dot -V
